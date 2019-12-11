88483a8d51ad6a5e215ba2ab216ba27b84b4ab46
JessLayton <jessylayton@yahoo.co.uk>
2019-12-11 15:05:35 +0000

    TeamPlayer domain updated

---
+++ src/main/java/com/bae/persistence/domain/TeamPlayer.java
@@ -0,0 +1,65 @@
+package com.bae.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TeamPlayer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String surname;
+
+	public TeamPlayer() {
+
+	}
+
+	public TeamPlayer(String firstName, String surname) {
+		this.firstName = firstName;
+		this.surname = surname;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	@Override
+	public String toString() {
+		return "TeamPlayer [id=" + id + ", firstName=" + firstName + ", surname=" + surname + "]";
+	}
+	
+	
+}
+
+		
+
+
+
+	
+		
+
+		
