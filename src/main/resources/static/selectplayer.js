35be1b50bc07983c64ccf4afb2c7eede4c1e9fc9
qa-admin <qa-admin@qaadmin-HP-ProDesk-400-G3-SFF>
2020-01-08 15:55:18 +0000

    initial front end files

---
+++ src/main/resources/static/selectplayer.js
@@ -0,0 +1,41 @@
+function createPlayer(){
+    var fName = document.getElementById("InputFirstName").value;
+    var sName = document.getElementById("InputSurname").value;
+    const url = 'http://localhost:8081/player/player';
+    var data = {"firstName": fName, "surname": sName};
+    axios.post(url, data
+    ).then((response) => {
+        console.log(response);
+        //redirect back
+    }).catch(e => {
+        console.log(e);
+        //refresh with ?error
+    });
+
+}
+
+function populateSelect(){ 
+    const url = 'http://localhost:8081/player/player';
+     
+    
+    axios.get(url
+    ).then((response) => {
+        response.data.forEach(addToScreen);
+        
+        console.log(response);
+        //redirect back
+    }).catch(e => {
+        console.log(e);
+        //refresh with ?error
+    });
+
+}
+
+function addToScreen(item, index){
+    var option = document.createElement("option");
+    var select = document.getElementById("playerList");
+    option.innerText = (item.firstName + " " + item.surname);
+    select.appendChild(option);
+}
+  
+  
