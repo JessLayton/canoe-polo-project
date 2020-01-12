04c2c5a92a966ca8d5c7eea3f1d26a3ab3ea20da
jessl <jessl@LAPTOP-QOVT6V18>
2020-01-12 18:05:01 +0000

    Set select option value equal to player id

--- src/main/resources/static/selectplayer.js
+++ src/main/resources/static/selectplayer.js
@@ -5,11 +5,11 @@ function createPlayer(){
     let sName = document.getElementById("InputSurname").value;
     const url = 'http://localhost:8080/player/player';
     let data = {"firstName": fName, "surname": sName};
-    axios.post(url, data
-    ).then((response) => {
-        console.log(response);
+    axios.post(url, data)
+    .then((response) => {
+        console.log(response);})
         //redirect back
-    }).catch(e => {
+    .catch(e => {
         console.log(e);
         //refresh with ?error
     });
@@ -20,24 +20,28 @@ function populateSelect(){
     const url = 'http://localhost:8080/player/player';
      
     
-    axios.get(url
-    ).then((response) => {
-        response.data.forEach(addToScreen);
-        
-        console.log(response);
-        //redirect back
-    }).catch(e => {
+    axios.get(url)
+    	.then((response) => {
+    		addToScreen(response.data);
+    		console.log(response);})
+    		//redirect back
+        .catch(e => {
         console.log(e);
         //refresh with ?error
-    });
+        });
 
-}
+	}
 
 function addToScreen(item, index){
-    let option = document.createElement("option");
-    let select = document.getElementById("playerList");
-    option.innerText = (item.firstName + " " + item.surname);
-    select.appendChild(option);
+	for (let x of item) {
+		let option = document.createElement("option");
+		    let select = document.getElementById("playerList");
+		    option.innerText = (x.firstName + " " + x.surname);
+		    option.value = x.id;
+		    console.log(x.id);
+		    select.appendChild(option);
+	}
+    
 }
   
   
