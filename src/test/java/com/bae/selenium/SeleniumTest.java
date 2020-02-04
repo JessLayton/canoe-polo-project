b6c3535261e61a9910ce720c79d414d8512b095d
jlayton <jlayton@jlayton-HP-ProDesk-400-G3-SFF>
2020-02-04 10:02:38 +0000

    selenium

--- src/test/java/com/bae/selenium/SeleniumTest.java
+++ src/test/java/com/bae/selenium/SeleniumTest.java
@@ -44,11 +44,11 @@ public class SeleniumTest {
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "chromedriver");
 		ChromeOptions options = new ChromeOptions();
-		options.setHeadless(true);
+		//options.setHeadless(true);
 		this.driver = new ChromeDriver(options);
 		this.driver.manage().window().maximize();
 		this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		this.addPlayerStage = new AddPlayerStage(this.driver);
+		this.addPlayerStage = new AddPlayerStage(this.driver);
 		this.addPlayerStage = PageFactory.initElements(this.driver, AddPlayerStage.class);
 		player.add("John");
 		player.add("Smith");
